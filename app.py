64d24c52f4096988db192435108272db4d381a06
RussOakham <rjoakham@gmail.com>
2021-04-16 16:49:07 +0000

    Logout - Python - added logout functionality and used jinja in nav to show different links if logged in/out

--- app.py
+++ app.py
@@ -85,7 +85,20 @@ def profile(username):
     # grab the session user's username from the db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("profile.html", username=username)
+
+    if session["user"]:
+        # If truthy
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookies
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
