5fd9372331cb7bd9d9067de91b7a4ee0c57c4f94
RussOakham <rjoakham@gmail.com>
2021-04-16 16:33:25 +0000

    profile - profile.html - HTML page created, placeholder content, python script updated

--- app.py
+++ app.py
@@ -47,6 +47,7 @@ def register():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
 
 
@@ -62,18 +63,29 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                    flash("Welcome, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
             else:
-                #invalid password
+                # invalid password
                 flash("Incorrect Username and/or Password")
                 return redirect(url_for("login"))
 
         else:
-        #username doesn't exist
+            # username doesn't exist
             flash("Incorrect Username and/or Password")
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/login/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session user's username from the db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
