b7eb5ed28238cc0f903f35ccf6b1ac83f3ec7605
RussOakham <rjoakham@gmail.com>
2021-04-19 15:00:44 +0000

    Categories - HTML - added categories page template

--- app.py
+++ app.py
@@ -125,6 +125,12 @@ def add_recipe():
     return render_template("add_recipe.html", categories=categories)
 
 
+@app.route("/get-categories")
+def get_categories():
+    categories = list(mongo.db.categories.find().sort("category_name", 1))
+    return render_template("categories.html", categories=categories)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
