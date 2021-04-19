62a29d6bcc20dbc58843da1c984b0a23d52683d7
RussOakham <rjoakham@gmail.com>
2021-04-19 14:39:33 +0000

    Styling - Navbar & Validation - Navbar updated to sticky-top, select validation added from code institute code

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,48 @@ $(document).ready(function () {
         edge: "right"
     });
     $('select').formSelect();
+
+    // Add Validate to 'Select' inputs in Materlize - Code from Code Instititue Task Manager Mini-Project
+    validateMaterializeSelect();
+
+    function validateMaterializeSelect() {
+        let classValid = {
+            "border-bottom": "1px solid #4caf50",
+            "box-shadow": "0 1px 0 0 #4caf50"
+        };
+        let classInvalid = {
+            "border-bottom": "1px solid #f44336",
+            "box-shadow": "0 1px 0 0 #f44336"
+        };
+        if ($("select.validate").prop("required")) {
+            $("select.validate").css({
+                "display": "block",
+                "height": "0",
+                "padding": "0",
+                "width": "0",
+                "position": "absolute"
+            });
+        }
+        $(".select-wrapper input.select-dropdown").on("focusin", function () {
+            $(this).parent(".select-wrapper").on("change", function () {
+                if ($(this).children("ul").children("li.selected:not(.disabled)").on("click", function () {})) {
+                    $(this).children("input").css(classValid);
+                }
+            });
+        }).on("click", function () {
+            if ($(this).parent(".select-wrapper").children("ul").children("li.selected:not(.disabled)").css("background-color") === "rgba(0, 0, 0, 0.03)") {
+                $(this).parent(".select-wrapper").children("input").css(classValid);
+            } else {
+                $(".select-wrapper input.select-dropdown").on("focusout", function () {
+                    if ($(this).parent(".select-wrapper").children("select").prop("required")) {
+                        if ($(this).css("border-bottom") != "1px solid rgb(76, 175, 80)") {
+                            $(this).parent(".select-wrapper").children("input").css(classInvalid);
+                        }
+                    }
+                });
+            }
+        });
+    }
 });
 
 // Add list item to ingredients list when '+' button hit
