09f2b2e19ea2ef7ad5e5c4f1b9753330326b5937
RussOakham <rjoakham@gmail.com>
2021-05-05 16:10:05 +0000

    star rating - updated star rating to use materialize icons to allow easier dynamic updating with functions

---
+++ user_rating.py
@@ -0,0 +1,10 @@
+from flask import session
+
+def calculate_avg_rating(rating):
+    # Calculate average rating from rating array.
+    sum_num = 0
+    for i in rating:
+        sum_num = sum_num + tuple
+
+    avg = sum_num / len(rating)
+    return avg
