ee8847effa100f5a1f3a005fe8ece2ebc4beb6e9
RussOakham <rjoakham@gmail.com>
2021-05-12 17:56:03 +0000

    user_management.py - pylint verification - added additional return statement for improved pylint scoring

--- user_management.py
+++ user_management.py
@@ -1,8 +1,8 @@
+""" Import functions needed for below scripts """
 from flask import session
 
-
 def user_logged_in():
-    # Returns if user is currently logged in
+    """ Returns if user is currently logged in """
     if session.get("user") is None:
         return False
 
@@ -10,14 +10,14 @@ def user_logged_in():
 
 
 def log_user_in(username):
-    # Adds user info to session cookie
+    """ Adds user info to session cookie """
     session['user'] = username['username']
     session['userid'] = str(username['_id'])
     session['userrole'] = username['role']
 
 
 def log_user_out():
-    # Removes the current user from the session cookie
+    """ Removes the current user from the session cookie """
     session.pop('user')
     session.pop('userid')
     session.pop('userrole')
