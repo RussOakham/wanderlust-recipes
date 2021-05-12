a58ea80c39847214196e75c71126b7cc24391905
RussOakham <rjoakham@gmail.com>
2021-05-12 16:15:23 +0000

    user_rating.py - pylint validation - docstrings added for validation

--- user_rating.py
+++ user_rating.py
@@ -1,12 +1,15 @@
+""" Calculate weighted average rating from rating vote count array."""
+
+
 def calculate_avg_rating(rating):
-    # Calculate weighted average rating from rating vote count array.
+    """ Set variables for us in function """
     cumulative = 0
-    sum = 0
+    sum_total = 0
 
-    for i in range(1,6):
+    for i in range(1, 6):
         cumulative += rating[i] * i
-        sum += rating[i]
+        sum_total += rating[i]
 
-    if sum > 0 and cumulative > 0:
-        average = round(cumulative / sum)
+    if sum_total > 0 and cumulative > 0:
+        average = round(cumulative / sum_total)
         return average
